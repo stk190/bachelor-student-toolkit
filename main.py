83a9c2d1b7e78601fdc25c2aafa648c27159572f
stk190 <alimalrazishatak@gmail.com>
2026-07-19 01:01:34 +0600

    Create ClI app startup

---
+++ main.py
@@ -0,0 +1,30 @@
+from modules.profile.profile import profile_menu
+
+def main():
+    while True:
+        
+        print ("Bacchelor Student Toolkit")
+        print ("1. Profile")
+        print ("2. Class  & Exam Schedule")
+        print ("3. Expemnse Tracker")
+        print ("4. Study Planner")
+        print ("5. Exit")
+
+        choice = input("\n Enter your choice (1-5): ")
+        if choice == '1':
+            print("Student Profile")
+            profile_menu()
+        elif choice == '2':
+            print("under development!!!")
+        elif choice == '3':
+            print("under development!!!")
+        elif choice == '4':
+            print("under development!!!")
+        elif choice == '5':
+            print("Exiting the program...")
+            break
+        else:
+            print("\n Invalid choice. Please try again.")
+
+if __name__ == "__main__":
+    main()  
---
+++ modules/profile/profile.py
@@ -0,0 +1,13 @@
+def profile_menu():
+    while True:
+        print("Student Profile & CGPA Manager")
+        print("1. Register Student")
+        print("2. View Student Profile")
+        print("3. Back")
+
+        choice = input("\nEnter your choice: ")
+
+        if choice == "3":
+            break
+
+        print("Feature coming soon...")
